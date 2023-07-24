32ce1fd28a31a88ce8eebaeeaae00cefbdf6a7b3
Naganandhini <135242443+Naganandhini10@users.noreply.github.com>
2023-07-24 22:45:50 +0530

    Add files via upload

---
+++ view.js
@@ -0,0 +1,32 @@
+// var swiper = new Swiper(".review-slider", {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     640: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     768: {
+//       slidesPerView: 4,
+//       spaceBetween: 40,
+//     },
+//     1024: {
+//       slidesPerView: 5,
+//       spaceBetween: 50,
+//     },
+//   },
+// });
+
+var swiper = new Swiper(".review-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
